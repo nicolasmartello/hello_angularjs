c364c9c08b974f8274af011e5499eb0cf1baa94f
nicolas martello <nicolasmartello@yahoo.com>
2013-11-28 17:23:14 -0300

    mensajmensajeemensaje

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,19 @@
 'use strict';
 angular.module('APP',[]).
   config(function ($routeProvider){
-    $routeProvider.
-    when('/',{ templateUrl: 'views/home.html'})
-  })
+    $routeProvider
+    .when('/',{templateUrl:'app/views/home.html', controller:'homeController'})
+    .when('/videos',{ templateUrl: 'app/views/video.html', controller:'videoController'})    
+    .otherwise({templateUrl: 'app/views/home.html'})
+  }).
+  controller('homeController',['$scope', function ($scope){
+  	activeMenu('home');
+  }]).
+  controller('videoController',['$scope', function ( $scope){
+	activeMenu('video');
+  }]);
+
+  function activeMenu(idLi){
+  	$('#menu li').removeClass('active');
+  	$('#'+idLi).addClass('active');
+  }
